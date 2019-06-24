bb6e7def7e66ccd613c24b88f8ffc40b76a9de16
ToddThomson <achilles@telus.net>
2019-06-24 11:45:09 -0700

    Fix node version in Travis build; Cleanup

--- run.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var TsCompiler_1 = require("./src/TsCompiler");
-var compileResult = TsCompiler_1.TsCompiler.compileProject("./src");
-console.log("Compilation completed.");
-console.log("File: ", compileResult.emitOutput[0].fileName);
-console.log("File Output: \n", compileResult.emitOutput[0].codeFile.data);
-//# sourceMappingURL=run.js.map
